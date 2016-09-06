8140c816e9d145c6efffdbb411307dc69d95730c
Neno Almeida <nenoxvx@icloud.com>
2016-09-06 08:56:54 +0000

    Thrird commit page_controller modifications

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,18 @@
 defmodule Chatapp.PageController do
   use Chatapp.Web, :controller
+  
+  plug :action
 
   def index(conn, _params) do
     render conn, "index.html"
   end
+  
+  def not_found(conn, _params) do
+  render conn, "not_found"
+  end
+  
+  def error(conn, _params) do
+  render conn, "error"
+  end
+  
 end
